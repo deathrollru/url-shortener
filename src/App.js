0ce93d1dde06dcc534def2d5b0087792f46d1f83
deathroll <deathroll@csgo.com>
2019-02-26 17:53:02 +0300

    Removed junk and front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div className="App-header">
+              <h1>URL Shortener</h1>
+              <form method="POST" action="/" name="shortenUrl" id="shortenUrlForm" className="shorten-form">
+                  <input id="url" name="url" type="text" placeholder="Paste a link to shorten it" className="shorten-input" />
+                  <input type="submit" value="Shorten!" className="shorten-button" />
+              </form>
+          </div>
       </div>
     );
   }
