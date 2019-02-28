de341175bab681626139d4b0d3d059f32695e555
deathroll <deathroll@csgo.com>
2019-02-28 08:54:49 +0300

    Updated package.json info and added ESlint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -11,11 +10,11 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-*/
-const express = require('express');
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+//
+// app.get('/', (req, res) => res.send('Hello World!'));
+//
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
