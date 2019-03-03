f821746d708171e993763b72412f96a5ab6d174d
deathroll <deathroll@csgo.com>
2019-03-03 11:21:01 +0300

    Modified React component and CSS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import Form from './form';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <div className="App-header">
-              <h1>URL Shortener</h1>
-              <form method="POST" action="/" name="shortenUrl" id="shortenUrlForm" className="shorten-form">
-                  <input id="url" name="url" type="text" placeholder="Paste a link to shorten it" className="shorten-input" />
-                  <input type="submit" value="Shorten!" className="shorten-button" />
-              </form>
-          </div>
+          <Form />
       </div>
     );
   }
