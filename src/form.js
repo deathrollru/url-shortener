53ca2af3bc4e5ac4c8fde10513ae3a3750fae22b
deathroll <deathroll@csgo.com>
2019-03-04 23:25:42 +0300

    View Label component and error, css, localStorage and etc.

--- src/form.js
+++ src/form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Label from './label';
 import './form.css';
 import validUrl from 'valid-url';
 import axios from 'axios';
@@ -6,8 +7,11 @@ import axios from 'axios';
 class Form extends Component {
     constructor(props) {
         super(props);
+        const localSt = JSON.parse(localStorage.getItem('urls'));
         this.state = {
-            value: ''
+            value: '',
+            urls: localSt ? localSt : [],
+            isError: false
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,37 +24,47 @@ class Form extends Component {
     handleSubmit(event) {
         event.preventDefault();
         if (!this.isUrl()) {
-            console.log('Not a URL');
+            this.setState({ isError: true });
             return false;
         }
-        this.test();
+        this.getShortenUrl();
+        this.setState({ value: '', isError: false });
     }
 
     isUrl() {
         return !!validUrl.isUri(this.state.value);
     }
 
-    test() {
-        axios.post('http://localhost:8000/', { url: this.state.value })
-        .then(function (response) {
+    getShortenUrl() {
+        axios.post('http://localhost:8000', { url: this.state.value })
+        .then((response) => {
             console.log(response);
+            this.setState(prevState => ({
+                urls: [...prevState.urls, response.data]
+            }));
+            localStorage.setItem('urls', JSON.stringify(this.state.urls));
         })
-        .catch(function (error) {
+        .catch((error) => {
             console.log(error);
         });
     }
 
     render() {
+        const { value, urls, isError } = this.state;
+        const errorMessage = 'Not a URL';
+
         return (
             <div className="App-header">
                 <h1>URL Shortener</h1>
                 <form onSubmit={this.handleSubmit} className="shorten-form">
-                    <input value={this.state.value}
+                    <input value={value}
                            onChange={this.handleChange}
                            type="text"
                            placeholder="Paste a link to shorten it"
                            className="shorten-input"
                     />
+                    { isError && errorMessage }
+                    {!isError && <Label urls={urls} />}
                     <input type="submit" value="Shorten!" className="shorten-button" />
                 </form>
             </div>
